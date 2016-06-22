bab639c6cb0d76964061e767c14f516d955f40d6
denniscual <mamaylovepapay@outlook.com>
2016-06-22 19:37:42 +0800

    Fifth Commit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,30 @@
-
-var bannerNo = 1,
-    bannerTotal = 3;
-
-var automaticSlider  = setInterval(function(){autoSlide()}, 8000);
-    //initilize the functions
+//DECLARTION OF THE VARIABLES
+//
+var myTimer = setInterval(function(){
+    autoSlide();
+  }, 8000);
+//initilize the functions
 $(document).ready(function(){
+
       movieSlider();
       left_rightControlSlider();
+      smoothScroll(1000);
+      changingColorNav();
+      toggleMenu();
+
 
 });
 
+function changingColorNav(){
+  $(window).scroll(function() { // check if scroll event happened
+    if ($(document).scrollTop() > 745) { // check if user scrolled more than 745 from top of the browser window
+      $("#head").addClass("change-nav");
+    } else {
+      $("#head").removeClass("change-nav") // if not
+    }
+  });
 
+}
 
 //movie SLIDER
 function movieSlider(){
@@ -33,14 +47,15 @@ function movieSlider(){
   $siblings.removeClass('active-slide'); //remove the class named on the slide-logo div.
   $this.addClass('active-slide'); //we will add class named active-slide on the div that we are clicking.
 
-
-  //clears a timer set with the setInterval() method.
-  clearInterval(automaticSlider);
-
   });
 }
 
 function left_rightControlSlider(){
+
+  $('.banner-belt').first().addClass('active-slide'); //default
+  $('.circle-control').first().addClass('active-slide');
+
+
   $('.left-control-prev, .right-control-next').click(function(){
 
       var $this            = $(this),
@@ -52,56 +67,95 @@ function left_rightControlSlider(){
       if($this.hasClass('right-control-next'))
       {
 
-          if(position < (movieNum - 1))
+          if(position < movieNum)
           {
             $('.active-slide').removeClass('active-slide').next().addClass('active-slide');
           }
           else{
             $('.banner-belt').removeClass('active-slide').first().addClass('active-slide');
             $('.circle-control').removeClass('active-slide').first().addClass('active-slide');
-
+            position = 0;
           }
+
+          console.log(position);
+
 
 
       }
       //left-control-prev function
       else{
 
-
-        if(position == 0){
-          $('.banner-blet').removeClass('active-slide').last().addClass('active-slide');
+        if((position-1) == 0)
+        {
+          $('.banner-belt').removeClass('active-slide').last().addClass('active-slide');
           $('.circle-control').removeClass('active-slide').last().addClass('active-slide');
-          position = 3;
         }
+
+
         else{
           $('.active-slide').removeClass('active-slide').prev().addClass('active-slide');
         }
 
+
+
       }
 
-      //clears a timer set with the setInterval() method.
-      clearInterval(automaticSlider);
 
 });
 
 }
 
 function autoSlide(){
+  var $this            = $(this),
+      curActiveMovie  = $('.header-wrapper').find('.active-slide'),
+      position         = $('.header-wrapper').children().index(curActiveMovie),
+      movieNum        = $('.banner-belt').length;
 
 
-  bannerNo = bannerNo + 1;
 
-  if(bannerNo > 0 && bannerNo <=3){
+  if(position < movieNum){
     //default function
     $('.active-slide').removeClass('active-slide').next().addClass('active-slide');
 
   }
-  else if(bannerNo > bannerTotal){
+  else{
     $('.banner-belt').removeClass('active-slide').first().addClass('active-slide');
     $('.circle-control').removeClass('active-slide').first().addClass('active-slide');
-    bannerNo = 1;
+    position = 0;
 
   }
 
+  console.log(position);
 
+}
+
+
+
+//smoothScroll function is applied from the document ready function
+function smoothScroll (duration)
+{
+	$('a[href^="#"').on('click', function(event)
+	{
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length )
+		{
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+				/* stuff to do after animation is complete */
+			},duration);
+		}
+
+	});
+}
+
+
+//slideIn and slideOout menu
+function toggleMenu(){
+  $(".menu").click(function(event){
+    event.preventDefault();//prevent the default callback function of an anchor tag.
+    $("#navMenu").toggleClass("active");
+  });
 }
